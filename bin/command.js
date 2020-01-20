ffd3c5071961e93b3b9b85298d1f5e08a490c68b
Kiksuru <tgvaporeon@gmail.com>
2020-01-20 12:54:29 -0600

    Redid the command architecture.

--- bin/command.js
+++ bin/command.js
@@ -3,48 +3,50 @@ const chalk = require("chalk");
 //Get the client and prefix from the main script.
 const {client,prefix} = require("../index.js");
 
-//Make a command count.
-var commandCount = 0;
-
 class BunCommand {
-    constructor(name,help,parameters,callback){
+    constructor(name,help,parameters,func){
         this.name = name;
         this.help = help;
+        this.para = parameters;
+        this.func = func;
 
-        //Initialise the command.
-        client.on("message", function(msg){
-            //If the content starts with the command..
-            if(msg.content.startsWith(prefix+name)){
-                //Log that a command execution has been attempted.
-                console.log(msg.author.tag+chalk.gray(" HAS EXECUTED ")+msg.content);
-
-                //Split the message.
-                var splt = msg.content.split(" ");
-                //Check if the parameters are set.
-                if(splt.length === parameters.length+1){
-                    //If they are, index the parameters into an object.
-                    var para = {};
-                    parameters.forEach(function(e,i){
-                        para[e] = splt[i+1];
-                    });
-
-                    //Finnaly, execute the command.
-                    callback(msg,para);
-                } else {
-                    //Send an error message.
-                    msg.reply("**Incorrect parameters.**\n\nParameters needed are:\n``"+parameters.join(" ")+"``");
-                    //Log an error message.
-                    console.log(chalk.red("ERROR: INCORRECT PARAMETERS"));
-                }
-            }
-        });
-
-        //Log the command that has been added.
-        console.log(chalk.gray("COMMAND ")+name+chalk.gray(" LOADED"));
-        //Increase the command count.
-        commandCount++;
+        //Log the command that has been created.
+        console.log(chalk.gray("COMMAND ")+name+chalk.gray(" CREATED"));
     }
 }
+
+//Start the command handler.
+client.on("message", function(msg){
+    //If the content starts with the prefix..
+    if(msg.content.startsWith(prefix)){
+        //Split the message.
+        var splt = msg.content.split(" ");
+        //Find the command.
+        var cmd = cmds.find(function(e){return prefix+e.name === splt[0]});
+
+        //If a command has been found..
+        if(cmd){
+            //Log that a command execution has been attempted.
+            console.log(msg.author.tag+chalk.gray(" HAS EXECUTED ")+msg.content);
+            //Check if the parameters are set.
+            if(splt.length === cmd.para.length+1){
+                //If they are, index the parameters into an object.
+                var para = {};
+                cmd.para.forEach(function(e,i){
+                    para[e] = splt[i+1];
+                });
+
+                //Finnaly, execute the command.
+                cmd.func(msg,para);
+            } else {
+                //Send an error message.
+                msg.channel.send("**Incorrect parameters.**\n\nParameters needed are:\n``"+cmd.para.join(" ")+"``");
+                //Log an error message.
+                console.log(chalk.red("ERROR: INCORRECT PARAMETERS"));
+            }
+        }
+    }
+});
 
 //Export BunCommand.
 module.exports.BunCommand = BunCommand;
@@ -77,5 +79,11 @@ const help = new BunCommand("help","Show tooltips and help on commands!",[],func
     msg.channel.send(helpStg);
 });
 
+//Make a command array.
+var cmds = [
+    help,
+    util.invite
+];
+
 //Export the command count.
-module.exports.commandCount = commandCount;
+module.exports.commandCount = cmds.length;
