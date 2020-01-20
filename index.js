6ac63570213677a79dbe3fd36e8a56495c50ca36
Kiksuru <tgvaporeon@gmail.com>
2020-01-20 01:55:18 -0600

    Added a Discord Client.

---
+++ index.js
@@ -0,0 +1,24 @@
+//Import the discord.js module.
+const Discord = require("discord.js");
+//Import the chalk module for some sweet colors.
+const chalk = require("chalk");
+
+//Create an instance of a Discord client.
+const client = new Discord.Client();
+
+//Set the prefix.
+const prefix = "b*"
+
+client.on("ready", function(){
+    console.log(chalk.green("BUNBOT API STARTED"));
+    console.log(chalk.gray("COMMANDS LOADED: ")+commandCount);
+    console.log(chalk.gray("LOGGED IN AS: ")+client.user.tag);
+});
+
+//Export the client and prefix for other scripts.
+module.exports = {client,prefix};
+
+//Grab the token.
+const token = require("./token.json");
+//Sign in using the token.
+client.login(token.token);
